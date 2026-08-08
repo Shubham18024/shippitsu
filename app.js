cab94d38bf1eb05a74e442bbdd94f5c897438d78
Shubham18024 <shubh.tiwari2244@gmail.com>
2026-08-08 23:47:37 +0530

    Initial project setup

---
+++ app.js
@@ -0,0 +1,19 @@
+import express from 'express';
+
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+const PORT = process.env.PORT || 3000;
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
+});
